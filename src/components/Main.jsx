6bebc8b21cf45d5e0c366797dcb4a14505849f2b
Ruinov <ruinov@hotmail.com>
2022-09-26 13:47:34 +0200

    Cambios para separar los componentes

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Test de casos2!!</Text>
+    </View>
+  );
+};
+
+export default Main;
