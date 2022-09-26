532cdab8c7233927f74ace968e196e82fbe81760
Ruinov <ruinov@hotmail.com>
2022-09-26 18:38:53 +0200

    Añadido tema

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import StyledText from './StyledText.jsx';
 
 const RepositoryItem = (props) => (
-  <View key={props.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
-    <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{props.id}</Text>
+  <View key={props.id} style={styles.container}>
+    <StyledText fontSize='subheading' fontWeight='bold'>
+      FullName: {props.fullName}
+    </StyledText>
+    <StyledText>Description: {props.description}</StyledText>
+    <StyledText>Language: {props.language}</StyledText>
+    <StyledText>Forks: {props.forksCount}</StyledText>
+    <StyledText>Stars: {props.stargazersCount}</StyledText>
+    <StyledText>Review: {props.reviewCount}</StyledText>
+    <StyledText>Rating: {props.ratingAverage}</StyledText>
+    <StyledText>Url: {props.ownerAvatarUrl}</StyledText>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+});
 
 export default RepositoryItem;
---
+++ src/components/StyledText.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import theme from '../theme.js';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: theme.fontSizes.body,
+    color: theme.colors.textPrimary,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal,
+  },
+  colorPrimary: {
+    color: theme.colors.primary,
+  },
+  colorSecondary: {
+    color: theme.colors.textSecondary,
+  },
+  bold: {
+    fontWeight: theme.fontWeights.bold,
+  },
+  subheading: {
+    fontSize: theme.fontSizes.subheading,
+  },
+});
+
+export default function StyledText({
+  children,
+  color,
+  fontSize,
+  fontWeight,
+  style,
+  ...restOfProps
+}) {
+  const textStyles = [
+    styles.text,
+    color === 'primary' && styles.colorPrimary,
+    color === 'secondary' && styles.colorSecondary,
+    fontSize === 'bold' && styles.subheading,
+    fontWeight === 'bold' && styles.bold,
+  ];
+  return (
+    <Text style={textStyles} {...restOfProps}>
+      {children}
+    </Text>
+  );
+}
